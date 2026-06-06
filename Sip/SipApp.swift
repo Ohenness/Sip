123282e9cc189cc0c7d92eac3bb4213785d671dc
Owen Hennessey <owenhenn@amazon.com>
2026-06-06 16:24:39 -0700

    Initial Commit

---
+++ Sip/SipApp.swift
@@ -0,0 +1,17 @@
+//
+//  SipApp.swift
+//  Sip
+//
+//  Created by Hennessey, Owen on 6/6/26.
+//
+
+import SwiftUI
+
+@main
+struct SipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SipTests/SipTests.swift
@@ -0,0 +1,19 @@
+//
+//  SipTests.swift
+//  SipTests
+//
+//  Created by Hennessey, Owen on 6/6/26.
+//
+
+import Testing
+@testable import Sip
+
+struct SipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
